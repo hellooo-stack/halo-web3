a6849be68a82de9484c6eb636a26469e04f198ea
Jeb.Wang <jeb.wannng@gmail.com>
2023-02-02 15:33:33 +0800

    Create TypeAddressTest.sol

---
+++ demos/TypeAddressTest.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract TypeAddressTest {
+
+//    编译报错，必须是一个合法的地址
+//    address private constant OWNER = abc;
+    address private constant OWNER = 0x8FeBa0F418561C038694f4e4Ba9168DE24E41B15;
+
+    function getOWNER() public returns (address) {
+        return OWNER;
+    }
+}
