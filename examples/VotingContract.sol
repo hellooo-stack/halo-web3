d0654763b3a686f15aa7bafdd4d1d69b63d2a0b2
Jeb.Wang <jeb.wannng@gmail.com>
2023-04-12 20:21:57 +0800

    Update VotingContract.sol

--- examples/VotingContract.sol
+++ examples/VotingContract.sol
@@ -6,15 +6,42 @@ pragma solidity ^0.8.0;
 //  - 用户可以投票，并且不能重复投票
 //  - 投票截止时间过后，合约会计算每个选项的得票数，并且返回获胜选项
 contract VotingContract {
-    function newVote(string topic, string[] options, uint deadline)  {
 
+    address public creator;
+    string public topic;
+    string[] public options;
+    uint public deadline;
+
+    mapping(address => bool) public hasVoted;
+    mapping(string => uint) public votes;
+
+    constructor(string _topic, string[] _options, uint _deadline) {
+        creator = msg.sender;
+        topic = _topic;
+        options = _options;
+
+//        todo deadline should be after the current time
+        deadline = _deadline;
     }
 
-    function vote(topic, topicOption) {
+    function vote(string option) public {
+        require(block.timestamp < deadline, "Voting has ended");
+        require(!hasVoted[msg.sender], "You have already vote!");
 
+        bool exists = false;
+        for (uint i = 0; i < options.length; i++) {
+            if (keccak256(bytes(options[i])) == keccak256(bytes(option))) {
+                exists = true;
+                break;
+            }
+        }
+
+        require(exists == true, "Option does not exist");
+        votes[option]++;
+        hasVoted[msg.sender] = true;
     }
 
-    function voteResult(topic) {
-
+    function getWinner(topic) public returns (string) {
+        
     }
 }
