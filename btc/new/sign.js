13d1dfb3bfaf8b9ff6d6a4b21e461f26d88f450d
Jeb.Wang <jeb.wannng@gmail.com>
2023-05-31 14:05:08 +0800

    update

--- btc/new/sign.js
+++ btc/new/sign.js
@@ -1,27 +1,24 @@
-const { ECPairFactory, networks } = require('ecpair');
+const {ECPairFactory, networks} = require('ecpair');
 const bitcoinMessage = require('bitcoinjs-message');
-
-// You need to provide the ECC library. The ECC library must implement
-// all the methods of the `TinySecp256k1Interface` interface.
 const tinysecp = require('tiny-secp256k1');
-// const network = require('')
-const ECPair = ECPairFactory(tinysecp);
-
-// const keyPair1 = ECPair.fromWIF('cQYcfMtZmtMFnTbEyucoDqaM9uSv2R2z1SCg4aJqQTL8JsMU7RnL', networks.testnet);
-const keyPair = ECPair.fromWIF('L4rK1yDtCWekvXuE6oXD9jCYfFNV2cWRpVuPLBcCU2z8TrisoyY1');
-
-const privateKey = keyPair.privateKey;
-const message = 'This is an example of a signed message.'
-
-const signature = bitcoinMessage.sign(message, privateKey, keyPair.compressed)
-console.log(signature.toString('base64'))
-
-const bitcoin = require('bitcoinjs-lib');
-const {address} = bitcoin.payments.p2pkh({pubkey: keyPair.publicKey});
-console.log('public: ' + keyPair.publicKey.toString('hex'));
-console.log('address: ' + address);
-
+const bitcoin = require("bitcoinjs-lib");
 
 function signMessageFromWIF(wif, message) {
+    const ECPair = ECPairFactory(tinysecp);
 
+    const keyPair = ECPair.fromWIF(wif);
+    const privateKey = keyPair.privateKey;
+    const signature = bitcoinMessage.sign(message, privateKey, keyPair.compressed)
+
+    const {address} = bitcoin.payments.p2pkh({pubkey: keyPair.publicKey});
+
+    console.log('address: ' + address);
+    console.log('public key: ' + keyPair.publicKey.toString('hex'));
+    console.log('signature: ' + signature.toString('base64'));
+
+    return signature.toString('base64');
 }
+
+// https://github.com/bitcoinjs/bitcoinjs-message
+signMessageFromWIF('L4rK1yDtCWekvXuE6oXD9jCYfFNV2cWRpVuPLBcCU2z8TrisoyY1', 'This is an example of a signed message.');
+// signMessageFromWIF('cQYcfMtZmtMFnTbEyucoDqaM9uSv2R2z1SCg4aJqQTL8JsMU7RnL', 'This is an example of a signed message.');
