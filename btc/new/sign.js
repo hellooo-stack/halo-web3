d0edf9bdfef06877fb3e5669c6ee1fc46148e387
Jeb.Wang <jeb.wannng@gmail.com>
2023-05-31 15:11:26 +0800

    update

--- btc/new/sign.js
+++ btc/new/sign.js
@@ -19,12 +19,6 @@ function signMessageFromWIF(wif, message) {
     const privateKey = keyPair.privateKey;
     const signature = bitcoinMessage.sign(message, privateKey, keyPair.compressed)
 
-    const {address} = bitcoin.payments.p2pkh({pubkey: keyPair.publicKey});
-
-    console.log('address: ' + address);
-    console.log('public key: ' + keyPair.publicKey.toString('hex'));
-    console.log('signature: ' + signature.toString('base64'));
-
     return signature.toString('base64');
 }
 
@@ -34,7 +28,6 @@ function verify(message, address, signature) {
         isValid = fallbackVerification(message, address, signature);
     }
 
-    console.log('is valid: ' + isValid);
     return isValid;
 }
 
@@ -43,10 +36,10 @@ function simpleVerification(message, address, signature) {
     try {
         // isValid = bitcoinMessage.verify(message, address, signature);
         isValid = bitcoinMessage.verify(message, address, signature, null, true);
-        console.log('before v: ' + isValid)
     } catch (e) {
         console.error(e);
     }
+
     return isValid;
 }
 
@@ -69,29 +62,30 @@ function fallbackVerification(message, address, signature) {
     return isValid;
 }
 
-// https://github.com/bitcoinjs/bitcoinjs-message
-// signMessageFromWIF('L4rK1yDtCWekvXuE6oXD9jCYfFNV2cWRpVuPLBcCU2z8TrisoyY1', 'This is an example of a signed message.');
-// signMessageFromWIF('cQYcfMtZmtMFnTbEyucoDqaM9uSv2R2z1SCg4aJqQTL8JsMU7RnL', 'hello world~');
-// // verify('This is an example of a signed message.', '1F3sAm6ZtwLAUnj7d38pGFxtP3RVEvtsbV', 'H9L5yLFjti0QTHhPyFrZCT1V/MMnBtXKmoiKDZ78NDBjERki6ZTQZdSMCtkgoNmp17By9ItJr8o7ChX0XxY91nk=');
-//
-// verify('hello world~', 'tb1qj2yrg95zt4sjqgtj9tu44d64xqe4m79rm89ne3', 'IA8Da70ACrUSmQiZ4zLzu9HVOWwGZFiYXFM3DtgtuONvKSXxojcJMSzIPs5qidfiD8gO6qBUm4Yyg9P81weFPBc=');
-
-
 
 // 1. p2pkh:
+function testP2PKH() {
 //   - address: 1341EtMhuQtcAUKruUomiacbu6WzYNHqxC
 //   - privateKey: KypVGiJzkHngK3aHg11Ra5A4p7FeKsg2dcCryqGxKzPhpwbCHdXx
 //   - generated from: https://www.bitaddress.org/
 // 1-1: test addressFromWIF
-const address = addressFromWIF('KypVGiJzkHngK3aHg11Ra5A4p7FeKsg2dcCryqGxKzPhpwbCHdXx');
-console.log('is parsing address equals address: ', address === '1341EtMhuQtcAUKruUomiacbu6WzYNHqxC');
+// ------check passed------
+    const wif = 'KypVGiJzkHngK3aHg11Ra5A4p7FeKsg2dcCryqGxKzPhpwbCHdXx';
+    const address = '1341EtMhuQtcAUKruUomiacbu6WzYNHqxC';
+    const extractedAddress = addressFromWIF(wif);
+    console.log('is parsing address equals address: ', extractedAddress === address);
 
+// ------check passed------
+// go to https://www.verifybitcoinmessage.com/ and check
+    const message = 'hello world';
+    const signature = signMessageFromWIF(wif, message);
+    console.log('signature: ' + signature);
 
+    const isValid = verify(message, address, signature);
+    console.log('isValid: ', isValid);
+}
 
-
-// signMessageFromWIF('KypVGiJzkHngK3aHg11Ra5A4p7FeKsg2dcCryqGxKzPhpwbCHdXx', 'hello world');
-
-
+testP2PKH();
 
 
 
