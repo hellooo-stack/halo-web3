c7d69af12667c60815ec2902062bd5afa82fa1f6
Jeb.Wang <jeb.wannng@gmail.com>
2023-05-31 14:28:44 +0800

    update

--- btc/new/sign.js
+++ btc/new/sign.js
@@ -58,7 +58,10 @@ function fallbackVerification(message, address, signature) {
 }
 
 // https://github.com/bitcoinjs/bitcoinjs-message
-signMessageFromWIF('L4rK1yDtCWekvXuE6oXD9jCYfFNV2cWRpVuPLBcCU2z8TrisoyY1', 'This is an example of a signed message.');
-verify('This is an example of a signed message.', '1F3sAm6ZtwLAUnj7d38pGFxtP3RVEvtsbV', 'H9L5yLFjti0QTHhPyFrZCT1V/MMnBtXKmoiKDZ78NDBjERki6ZTQZdSMCtkgoNmp17By9ItJr8o7ChX0XxY91nk=');
+// signMessageFromWIF('L4rK1yDtCWekvXuE6oXD9jCYfFNV2cWRpVuPLBcCU2z8TrisoyY1', 'This is an example of a signed message.');
+// verify('This is an example of a signed message.', '1F3sAm6ZtwLAUnj7d38pGFxtP3RVEvtsbV', 'H9L5yLFjti0QTHhPyFrZCT1V/MMnBtXKmoiKDZ78NDBjERki6ZTQZdSMCtkgoNmp17By9ItJr8o7ChX0XxY91nk=');
 
-// verify('hello world~', 'tb1qj2yrg95zt4sjqgtj9tu44d64xqe4m79rm89ne3', 'HL4McV4Xyd2bNs7m3wytVU5m0aqYeDDtVetBYC3FZ1t0fd+W9Gi6y9HG6e1WKVw5ANtM9ck/m1Acz0BpW1Y6gjg=');
+verify('hello world~', 'tb1qj2yrg95zt4sjqgtj9tu44d64xqe4m79rm89ne3', 'HL4McV4Xyd2bNs7m3wytVU5m0aqYeDDtVetBYC3FZ1t0fd+W9Gi6y9HG6e1WKVw5ANtM9ck/m1Acz0BpW1Y6gjg=');
+
+
+// 1. Legacy address: sign done, verify done.
