5e060492de6826546888cc939ba02e48aa42a578
Jeb.Wang <jeb.wannng@gmail.com>
2023-05-31 11:20:04 +0800

    update

---
+++ btc/valid.js
@@ -0,0 +1,47 @@
+const bitcoinMessage = require('bitcoinjs-message');
+
+// 自己的测试账号 unisat生成的sign
+let message = 'hello world~';
+let address = 'tb1qj2yrg95zt4sjqgtj9tu44d64xqe4m79rm89ne3';
+let signature = 'GxgtooYWXtBOoAPhQQHzCleEvfB3rsJ0XL4QZdyFQxD2Qs3JviIDhJA8CpO1JtdZpNYlrw6thAYoJLioyBkZvTc=';
+
+// 自己的测试账号 用java代码生成的sign
+// let message = 'hello world';
+// let address = 'tb1qj2yrg95zt4sjqgtj9tu44d64xqe4m79rm89ne3';
+// let signature = 'H7JJTwrfed+eih5feDitoT1HRopdsi+61l+NMOHizF2UFfL5ljXxolYnjKDhq/MAHO4urqmb7EYijH5YZjHk/8Y=';
+
+
+let isValid = false;
+isValid = simpleVerification(message, address, signature);
+if (!isValid) {
+    isValid = fallbackVerification(message, address, signature);
+}
+console.log('is valid: ' + isValid);
+
+function simpleVerification(message, address, signature) {
+    let isValid = false;
+    try {
+        isValid = bitcoinMessage.verify(message, address, signature);
+    } catch (e) {
+    }
+    return isValid;
+}
+
+function fallbackVerification(message, address, signature) {
+    let isValid = false;
+    const flags = [...Array(12).keys()].map(i => i + 31);
+    for (let flag of flags) {
+        let flagByte = Buffer.alloc(1);
+        flagByte.writeInt8(flag);
+        let sigBuffer = Buffer.from(signature, 'base64').slice(1);
+        sigBuffer = Buffer.concat([flagByte, sigBuffer]);
+        let candidateSig = sigBuffer.toString('base64');
+        try {
+            isValid = bitcoinMessage.verify(message, address, candidateSig);
+            if (isValid) break;
+        } catch (e) {
+            console.error(e);
+        }
+    }
+    return isValid;
+}
