bcc14105b2b9ccb600989a3c1ecb26e453da0b01
Jeb.Wang <jeb.wannng@gmail.com>
2023-07-24 16:19:15 +0800

    Update learnbtc.js

--- btc/learnbtc.js
+++ btc/learnbtc.js
@@ -1,16 +1,3 @@
-// wif privateKey: L4dkwu8AbcNG85QeDRPRJndjPavG6aNPC1SvHuzh1mSA3VF7SKQD
-// hex privateKey: dd3b981ee923c50ca27b6bd22055491750a460ae888edfc2ee022c17ee5efd61
-// main-net:
-//   - p2tr: bc1p8y2vtya493kyuzpewgv0qt429t3z7y3gfcmc3eau0ymsh523zzzswz7vyg
-//   - p2wpkh: bc1qrv4dd6vud35926qlt4s39kqlsy3a3r8kqeydyy
-//   - p2pkh: 1MU3iZcH4p3QNbrrs4p8b2ocjP4cFYvBCj
-// test-net:
-//   - p2tr: tb1p8y2vtya493kyuzpewgv0qt429t3z7y3gfcmc3eau0ymsh523zzzse2gr78
-//   - p2wpkh: tb1qrv4dd6vud35926qlt4s39kqlsy3a3r8k2ll7lh
-//   - p2pkh: n1z11chFsqUf9iLUadnWQx1wbNfK7JqVQ2
-// 以上是从unisat钱包导出的，单个HD Wallet在不同网络下的不同形式地址，
-// 需要验证：是否同一私钥，在不同网络下可以产生不同形式的地址。
-
 // 1. 生成私钥
 
 
