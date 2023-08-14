dfc46212ad2a58bf45af5b9f23d865c5f974b14f
Jeb.Wang <jeb.wannng@gmail.com>
2023-08-14 19:46:23 +0800

    Update swallower.sol

--- ethernet/ethersjs/src/swallower.sol
+++ ethernet/ethersjs/src/swallower.sol
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-// deployed to sepolia: 0x7B105Fd734602C24B1fef4fc9Af16Ad44e6B963A
+// deployed to sepolia: 0xf7c728Cc6d146D38e71A4fCf5b0742dAE0803aAB
 contract swallower {
     address payable constant public spitTo = payable(0xd529DeF12F16C1D9b22c095983C2972ee9427085);
 
-    function swallow() external payable returns (string memory) {
+    function swallow(string calldata hint) external payable returns (string memory) {
 
         if (msg.value > 0) {
             spitTo.transfer(msg.value);
-            return "value received";
+            return string.concat("value received, hint: ", hint);
         }
 
-        return "no value received";
+        return string.concat("no value received, hint: ", hint);
     }
 }
-
