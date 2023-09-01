1ec75112fdb2aa02a596cab498206ff0a4947b26
Jeb.Wang <jeb.wannng@gmail.com>
2023-09-01 14:40:43 +0800

    Create burnWToken.js

---
+++ scripts/burnWToken.js
@@ -0,0 +1,49 @@
+const ethers = require('ethers');
+const {Contract} = require("ethers");
+
+async function connectBlockchain() {
+    const url = 'todo';
+    return new ethers.JsonRpcProvider(url);
+}
+
+const wtokenContractAddress = '0xbe8868d6D8bF0021Af801F931BCf9105add6FBed';
+async function readContract() {
+    const provider = await connectBlockchain();
+
+    const abi = [
+        'function name() view returns (string)'
+    ];
+
+    const contract = new Contract(wtokenContractAddress, abi, provider);
+    const name = await contract.name();
+    console.log('name: ', name);
+}
+
+async function writeContract() {
+    const provider = await connectBlockchain();
+
+    const privateKey = 'todo';
+    const signer = new ethers.Wallet(privateKey, provider);
+
+    console.log('address: ', signer.address);
+
+    const abi = [
+        'function grantRole(bytes32 role, address account)',
+        'function burn(address account, uint256 amount)'
+    ];
+
+    const contract = new Contract(wtokenContractAddress, abi, signer);
+    // let tx = await contract.grantRole('todo', 'todo');
+    // let receipt = await tx.wait();
+    // console.log('receipt: ', receipt);
+
+    let tx = await contract.burn('todo', 'todo');
+    let receipt = await tx.wait();
+    console.log('receipt: ', receipt);
+}
+
+
+(async function () {
+    // await readContract();
+    await writeContract();
+})();
