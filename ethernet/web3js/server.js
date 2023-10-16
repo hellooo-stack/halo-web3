14c86325645985a3ebfed34d531881ca1b9f79a8
Jeb.Wang <jeb.wannng@gmail.com>
2023-10-16 19:58:37 +0800

    reorganize codes of ethernet

--- ethernet/web3js/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, '.')));
-
-app.listen(8097, () => {
-    console.log('Server started on port 8097');
-});
